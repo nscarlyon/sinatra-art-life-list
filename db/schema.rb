c540fd44a2567be086808bf99e2e419860d764e3
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-11 21:39:20 +0000

    created migration for users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711184822) do
+ActiveRecord::Schema.define(version: 20160711213629) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 20160711184822) do
 
   create_table "movements", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
