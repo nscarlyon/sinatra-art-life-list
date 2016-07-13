689f9b22cd3269bbc132b0624c54cbc38a62c316
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-13 00:43:17 +0000

    artists index shows all artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,11 @@
 class ArtistsController < ApplicationController
 
   get '/artists' do
-    erb :'artists/index'
+    if logged_in?
+      erb :'artists/index'
+    else
+      redirect to "/login"
+    end
   end
+
 end
