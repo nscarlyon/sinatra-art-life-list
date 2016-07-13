76a8db0aa7d8d839ae76b65a7f50e624faec996e
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-13 00:54:27 +0000

    if/else state for movements index route; movements show page

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,6 +1,17 @@
 class MovementsController < ApplicationController
 
   get '/movements' do
-    erb :'movements/index'
+    if logged_in?
+      erb :'movements/index'
+    else
+      redirect to "/login"
+    end
   end
+
+  get '/movements/:slug' do
+    @movement = Movement.find_by_slug(params[:slug])
+    erb :'movements/show'
+  end
+
+
 end
