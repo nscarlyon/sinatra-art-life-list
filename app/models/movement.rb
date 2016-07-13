65aeec3206de7c6ab4410b51604d96f7aaacb033
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-13 00:52:12 +0000

    added slug methods to movement

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -3,4 +3,12 @@ class Movement < ActiveRecord::Base
   has_many :artworks, through: :artwork_movements
   has_many :artists, through: :artworks
 
+  def slug
+    self.name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    Movement.all.find{|a| a.slug == slug}
+  end
+
 end
