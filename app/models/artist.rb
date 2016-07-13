0046bcb3128c47616fc82c668ce3401dbbf1775d
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-13 00:38:24 +0000

    added slug methods to artist class

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,13 @@
 class Artist < ActiveRecord::Base
   has_many :artworks
   has_many :movements, through: :artworks
+
+  def slug
+    self.name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    Artist.all.find{|a| a.slug == slug}
+  end
+
 end
